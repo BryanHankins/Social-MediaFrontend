a4a06b9b9ac358df153933f1a2024999d71a8c72
Bryan <bryanhankinsdev@gmail.com>
2024-08-14 10:28:08 -0500

    first commit

--- src/pages/Discover2.tsx
+++ src/pages/Discover2.tsx
@@ -1,6 +1,6 @@
 
-import React, { useState } from 'react';
-import Footer from '../components/Footer'
+import React from 'react';
+import Footer from '../components/Footer';
 
 const Discover2: React.FC = () => {
  
